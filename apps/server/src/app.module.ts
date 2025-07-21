8bdaed496d191a5d017f73ac63042a6bc9a1f401
전성훈 / SUNG HOON CHON <letaem77@gmail.com>
2025-07-21 10:12:04 +0900

    feat: Setup NestJS ConfigModule
    Signed-off-by: 전성훈 / SUNG HOON CHON <letaem77@gmail.com>

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 
 import { AIChatModule } from './ai-chat/ai-chat.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AIChatModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }), AIChatModule],
 })
 export class AppModule {}
