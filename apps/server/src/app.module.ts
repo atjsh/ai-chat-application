ab8cb2892bb13026a6b5fba20807e50b1cbfadf1
전성훈 / SUNG HOON CHON <letaem77@gmail.com>
2025-08-17 22:07:33 +0900

    feat: AIChatSessionModule이 Swagger에 표시되도록 설정 변경
    Signed-off-by: 전성훈 / SUNG HOON CHON <letaem77@gmail.com>

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
-
-import { AIChatModule } from './ai-chat/ai-chat.module';
 import { ConfigModule } from '@nestjs/config';
 
+import { AIChatSessionModule } from './ai-chat-session/ai-chat-serssion.module';
+import { AIChatModule } from './ai-chat/ai-chat.module';
+
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }), AIChatModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }), AIChatModule, AIChatSessionModule],
 })
 export class AppModule {}
--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,13 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: '*',
-    },
-  });
+  const app = await NestFactory.create(AppModule, { cors: { origin: '*' } });
+
+  const config = new DocumentBuilder().setTitle('AI Chat API').addBearerAuth().build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('openapi', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
