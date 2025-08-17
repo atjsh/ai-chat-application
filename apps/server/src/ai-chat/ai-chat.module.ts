692acaf58c9bd66ba02820e29477b50352d5476f
전성훈 / SUNG HOON CHON <letaem77@gmail.com>
2025-08-17 22:07:14 +0900

    feat: AIChatGateway에 AIChatSessionService 비즈니스 로직 추가
    Signed-off-by: 전성훈 / SUNG HOON CHON <letaem77@gmail.com>

--- apps/server/src/ai-chat/ai-chat.module.ts
+++ apps/server/src/ai-chat/ai-chat.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 
 import { ChatSessionModule } from '../core/chat-session/chat-session.module';
+import { AIChatSessionModule } from '../ai-chat-session/ai-chat-serssion.module';
 
 import { AIChatGateway } from './connectors/ai-chat.gateway';
 
 @Module({
-  imports: [ChatSessionModule],
+  imports: [ChatSessionModule, AIChatSessionModule],
   providers: [AIChatGateway],
 })
 export class AIChatModule {}
--- apps/server/src/ai-chat/connectors/ai-chat.gateway.ts
+++ apps/server/src/ai-chat/connectors/ai-chat.gateway.ts
@@ -1,13 +1,24 @@
-import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
-import { Server } from 'socket.io';
+import { WebSocketGateway, WebSocketServer, OnGatewayConnection } from '@nestjs/websockets';
+import { Server, Socket as SocketIOClient } from 'socket.io';
 
-@WebSocketGateway({
-  cors: {
-    origin: '*',
-    credentials: true,
-  },
-})
-export class AIChatGateway {
+import { AIChatSessionService } from '../../ai-chat-session/ai-chat-session.service';
+
+@WebSocketGateway({ cors: { origin: '*', credentials: true } })
+export class AIChatGateway implements OnGatewayConnection {
   @WebSocketServer()
   server: Server;
+
+  constructor(private readonly aiChatSessionService: AIChatSessionService) {}
+
+  handleConnection(client: SocketIOClient) {
+    const token = client.handshake.auth.token;
+    if (typeof token !== 'string') {
+      client.disconnect();
+      return;
+    }
+    const isValid = this.aiChatSessionService.verifyEphemeralAPIToken(token);
+    if (!isValid) {
+      client.disconnect();
+    }
+  }
 }
